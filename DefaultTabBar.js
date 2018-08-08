2b5af9f4050b27c6c30367cdc4f861744a29cb05
Sam Cheng <samchengmc@redso.com.hk>
2018-08-08 15:30:46 +0800

    Update tab bar height

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -102,7 +102,7 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
   },
   tabs: {
-    height: 50,
+    height: 45,
     flexDirection: 'row',
     justifyContent: 'space-around',
     borderWidth: 1,
